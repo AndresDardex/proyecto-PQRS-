3fddb7852d8d7e23e015e5d1a2192a952beeb815
daniel.torres6 <daniel.torres@welli.com.co>
2025-04-18 16:52:14 -0500

    feat: añadir estructura inicial para lógica de autenticación

--- PQRS_Sistem/urls.py
+++ PQRS_Sistem/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import registrar_cliente
+from core.views import registrar_cliente, login_personalizado, vista_cliente, vista_gestor, cerrar_sesion
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='inicio.html'), name='inicio'),
     path('registrar/', registrar_cliente, name='registrar_cliente'),
+    path('iniciar-sesion/', login_personalizado, name='login'),
+    path('cliente/', vista_cliente, name='vista_cliente'),
+    path('gestor/', vista_gestor, name='vista_gestor'),
+    path('logout/', cerrar_sesion, name='cerrar_sesion'),
 ]
