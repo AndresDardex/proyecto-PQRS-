97a03ed3ee973ff78cc48e66b58959c32a5a3da1
Braian Felipe Ramírez Ortiz <bfrosm@hotmail.com>
2025-05-03 08:12:39 -0500

    feat: eliminación de la carpeta staticfiles

--- PQRS_Sistem/urls.py
+++ PQRS_Sistem/urls.py
@@ -37,5 +37,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Handlers de errores personalizados
-handler404 = 'core.views.error_404'
-handler500 = 'core.views.error_500'
+#handler404 = 'core.views.error_404'
+#handler500 = 'core.views.error_500'
